f287ebe93cabbd312da42431f54a46a037b8818c
Jonathan Cardenas <jonscardenas@gmail.com>
2021-11-26 14:57:40 -0600

    added json server, and user class methods on and trigger

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,16 @@ import { User } from "./models/User";
 
 const user = new User({name: 'jontan', age: 20});
 
-console.log(user.get('name'));
-console.log(user.get('age'));
+user.on('change', () => {
+    console.log('change #1');
+});
+user.on('change', () => {
+    console.log('change #2');
+});
+user.on('save', () => {
+    console.log('save was triggered');
+});
+
+
+
+user.trigger('asdasdasd');
--- src/models/User.ts
+++ src/models/User.ts
@@ -1,14 +1,43 @@
 interface UserProps {
-    name: string,
-    age: number
+    //question marks make properties optional
+    name?: string,
+    age?: number
 }
+
+//type alias used for type annotation
+type Callback = () => void;
 
 
 export class User {
+    events: {[key: string]: Callback[] } = {};
+
     // set to private so other engineers can't just access user data
     constructor(private data: UserProps){}
 
     get(propName: string): (number | string) {
         return this.data[propName];
+    }
+
+    set(update: UserProps): void {
+
+        Object.assign(this.data, update);
+    }
+
+    on(eventName: string, callback: Callback): void {
+        const handlers = this.events[eventName] || [];
+        handlers.push(callback);
+        this.events[eventName] = handlers;
+    }
+
+    trigger(eventName: string): void {
+        const handlers = this.events[eventName];   
+
+        if (!handlers || handlers.length === 0) {
+            return;
+        }
+
+        handlers.forEach(callback => {
+            callback();
+        })
     }
 }
