33e2f5af4196618c22ca83367521276234e32867
Jonathan Cardenas <jonscardenas@gmail.com>
2021-11-29 16:32:27 -0600

    finished user.ts refactor

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,4 +1,8 @@
 import { Model } from './Model';
+import { Attributes } from './Attributes';
+import { ApiSync } from './ApiSync';
+import { Eventing } from './Eventing';
+
 
 export interface UserProps {
     //question marks make properties optional
@@ -10,6 +14,12 @@ export interface UserProps {
 const rootUrl = 'http://localhost:3000/users';
 
 export class User extends Model<UserProps> {
-  
+    static buildUser(attrs: UserProps): User {
+        return new User(
+            new Attributes<UserProps>(attrs),
+            new Eventing(),
+            new ApiSync<UserProps>(rootUrl)
+        )
+    }
     
 }
