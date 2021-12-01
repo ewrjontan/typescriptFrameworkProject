7273a2a1cd7f1c39795a4c45fe29475cd603869c
Jonathan Cardenas <jonscardenas@gmail.com>
2021-12-01 14:14:29 -0600

    extended generic constraints in View.ts, Models and UserForm.ts

--- src/views/UserForm.ts
+++ src/views/UserForm.ts
@@ -1,7 +1,7 @@
-import { User } from '../models/User';
+import { User, UserProps } from '../models/User';
 import { View } from './View';
 
-export class UserForm extends View{
+export class UserForm extends View<User, UserProps>{
 
     //object array of keys that takes no arguments and returns nothing
     eventsMap(): { [key: string]: () => void } {
--- src/views/View.ts
+++ src/views/View.ts
@@ -1,8 +1,10 @@
 import { User } from "../models/User";
+import { Model } from '../models/Model';
 
-export abstract class View {
 
-    constructor(public parent: Element, public model: User){
+export abstract class View<T extends Model<K>, K>{
+
+    constructor(public parent: Element, public model: T){
         this.bindModel();
     }
 
